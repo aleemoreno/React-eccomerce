1c75c33e8eb078df08b0774ec50db6d8d2e6ca66
Alejandro Moreno <alejandromoreno02@live.com.ar>
2024-10-08 19:17:32 -0300

    React-eccomerce

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CartWidget = () => {
+  return (
+    <div>
+      <img src="cart-icon.png" alt="Carrito de compras" />
+      <span>3</span> {}
+    </div>
+  );
+};
+
+export default CartWidget;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,12 @@
+import React from 'react';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 
 function App() {
-
-  const mensaje = "Bienvenido a nuestra tienda!";
-
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greeting={"Bienvenido a nuestra tienda!"} />
+      <ItemListContainer greeting="Bienvenido a nuestra tienda!" />
     </div>
   );
 }
